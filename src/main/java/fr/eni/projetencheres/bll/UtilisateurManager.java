7ebf860f3653fdeb0a2f93239b667f4ef6b24fe6
Jeromecda <jerome1.vasseur@yahoo.fr>
2022-04-12 14:19:28 +0200

    ADD : bll

---
+++ src/main/java/fr/eni/projetencheres/bll/UtilisateurManager.java
@@ -0,0 +1,21 @@
+package fr.eni.projetencheres.bll;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import fr.eni.projetencheres.bll.BusinessException;
+import fr.eni.projetencheres.bo.Utilisateur;
+import fr.eni.projetencheres.dal.DAOFactory;
+import fr.eni.projetencheres.dal.UtilisateurDAO;
+
+public class UtilisateurManager {
+	
+	private UtilisateurDAO utilisateurDAO = DAOFactory.getUtilisateurDAO();
+	
+	
+	private void validation(Utilisateur utilisateur) throws BusinessException{
+		
+
+	}
+}
