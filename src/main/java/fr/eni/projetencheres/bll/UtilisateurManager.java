b6f43cf241515b55bb9b8762a419f42d9b45fe20
Jeromecda <jerome1.vasseur@yahoo.fr>
2022-04-13 10:18:05 +0200

    Add : Méthode verifierMotDePasseEtLogin (#1001)

--- src/main/java/fr/eni/projetencheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projetencheres/bll/UtilisateurManager.java
@@ -14,11 +14,44 @@ import fr.eni.projetencheres.dal.UtilisateurDAO;
 public class UtilisateurManager {
 	
 	private UtilisateurDAO utilisateurDAO = DAOFactory.getUtilisateurDAO();
-	
-	
-	private void validation(Utilisateur utilisateur) throws BusinessException, SQLException {
-		if( utilisateur.getPseudo() != null ) {
-			this.utilisateurDAO.add(utilisateur);
-		}	
+
+	private Utilisateur trouverUtilisateur(String login, String password) throws BusinessException, SQLException {
+		
+		Utilisateur utilisateur = new Utilisateur();
+		return utilisateur;
 	}
+	
+	private void verifierMotDePasseEtLogin(String login, String password) throws BusinessException, SQLException {
+		
+		
+		
+		if(login.length() < 3) {
+			throw new BusinessException("Le login doit comporter au moins 3 lettres");
+		}
+		if(password.length() < 3) {
+			throw new BusinessException("Le mot de passe doit comporter au moins 3 lettres");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	private void validationConnexion(String login, String password) throws BusinessException, SQLException {
+//
+//		try {
+//			Utilisateur utilisateur = new Utilisateur();
+//			
+//			if( login != null && password != null ) {
+//				this.utilisateurDAO.check(utilisateur);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			throw new BusinessException("erreur SQL lors de l'insertion en base de donnée");
+//		}		
+//	}
 }
