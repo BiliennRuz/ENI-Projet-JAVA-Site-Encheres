d58f44b5947c0d24eb4f2483b2bca22493bbe065
BiliennRuz <62357926+BiliennRuz@users.noreply.github.com>
2022-04-13 14:59:42 +0200

    ADD: DAO Article et rename fct UtilisateurDAO

--- src/main/java/fr/eni/projetencheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/projetencheres/dal/UtilisateurDAO.java
@@ -8,8 +8,8 @@ import fr.eni.projetencheres.bo.Utilisateur;
 public interface UtilisateurDAO {
 
 	public List<Utilisateur> getUser() throws SQLException;
-	public void add(Utilisateur user) throws SQLException;
-	public void update(Utilisateur user) throws SQLException;
+	public void addUser(Utilisateur user) throws SQLException;
+	public void updateUser(Utilisateur user) throws SQLException;
 	public Utilisateur checkConnectUser(String login, String password) throws SQLException;
 
 }
